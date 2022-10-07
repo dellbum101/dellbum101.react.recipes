b0d64170567bb5d9d6bc5665e3c7850269617b57
Kellen Rogers <kellenr@keiser.com>
2022-10-06 20:52:15 -0700

    changed search svg to material ui icon

--- components/ListSelectionFilterWithSearch.tsx
+++ components/ListSelectionFilterWithSearch.tsx
@@ -1,6 +1,6 @@
+import { Search } from '@mui/icons-material'
 import React, { useCallback, useState } from 'react'
 import { RecipeDishTypes } from '../apis/recipes/recipe.types'
-import PopUpMenu from './PopUpMenu'
 
 type Options = { title: string; value: string }
 
@@ -39,19 +39,7 @@ const ListSelectionFilterWithSearch = ({
             <span className="font-bold text-lg mb-3">{filterTitle}</span>
             <div className="w-80 relative md:block">
               <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                <svg
-                  className="w-5 h-5 text-gray-500"
-                  aria-hidden="true"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
+                <Search />
                 <span className="sr-only">Search icon</span>
               </div>
               <input
